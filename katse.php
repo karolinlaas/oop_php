ec57e901a112bf7df34cfaabbfca11d30b78c862
karolin.laas <karolinlaas@hotmail.com>
2018-12-11 08:42:22 +0200

    PHP koodi katsetamine

---
+++ katse.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karolin.laas
+ * Date: 06.12.2018
+ * Time: 11:08
+ */
+
+echo '
+    <!doctype html>
+    <html>
+        <head>
+            <title></title>
+        </head> 
+        <body>
+            <h1>PHP abil loodud HTML</h1>
+        </body> 
+    </html>
+';
+
+?>
