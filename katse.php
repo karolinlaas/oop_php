f6f2de82f80fb9ce6a994729390f69ab3f13b48c
karolin.laas <karolinlaas@hotmail.com>
2018-12-11 09:56:48 +0200

    tingimuslause

--- katse.php
+++ katse.php
@@ -22,35 +22,48 @@ $toevaartusTrue = true;
 $toevaartusFalse = false;
 // väärtuse tüübi kontroll - gettype($muutujaNimi)
 // väärtuse tüübi teisendus - settype($muutujaNimi)
-// teksti väljastamine
 // ühendamine 'tekst katkeb '.$muutjaNimi.' tekst jätkub'
-echo '
+// tingimuslause
+/*
+ * if  (tingimus == true) {
+ *      siis toimub see osa
+ * } else {
+ *      muidu toimub see osa
+ * }
+ * */
+$naitaSisu = true;
+// teksti väljastamine
+    echo '
     <!doctype html>
     <html>
         <head>
-            <title> '.$lehePealkiri.' </title>
+            <title> ' . $lehePealkiri . ' </title>
         </head> 
-        <body>
-            <h1> '.$sisuPealkiri.' </h1>
-            <h3>Andmetüübid</h3>
-            <ul>
-                <li>$taisarv = '.$taisarv.' - '.gettype($taisarv).'</li>
-                <li>$reaalarv = '.$reaalarv.' - '.gettype($reaalarv).'</li>
-                <li>$tekst = '.$tekst.' - '.gettype($tekst).'</li>
-                <li>$toevaartusTrue = '.$toevaartusTrue.' - '.gettype($toevaartusTrue).'</li>
-                <li>$toevaartusFalse = '.$toevaartusFalse.' - '.gettype($toevaartusFalse).'</li>
-            </ul>
-            ';
-// tüübi teisendamise katse
-echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
-echo '<br>';
-settype($taisarv, 'string');
-echo $taisarv.' + '.$taisarv.' = '.($taisarv + $taisarv);
-echo '<br>';
-echo gettype($taisarv);
-echo '      
+        <body>';
+
+    if($naitaSisu == true) {
+    echo '
+                    <h1> ' . $sisuPealkiri . ' </h1>
+                    <h3>Andmetüübid</h3>
+                    <ul>
+                        <li>$taisarv = ' . $taisarv . ' - ' . gettype($taisarv) . '</li>
+                        <li>$reaalarv = ' . $reaalarv . ' - ' . gettype($reaalarv) . '</li>
+                        <li>$tekst = ' . $tekst . ' - ' . gettype($tekst) . '</li>
+                        <li>$toevaartusTrue = ' . $toevaartusTrue . ' - ' . gettype($toevaartusTrue) . '</li>
+                        <li>$toevaartusFalse = ' . $toevaartusFalse . ' - ' . gettype($toevaartusFalse) . '</li>
+                    </ul>
+                    ';
+            // tüübi teisendamise katse
+            echo $taisarv . ' + ' . $taisarv . ' = ' . ($taisarv + $taisarv);
+            echo '<br>';
+            settype($taisarv, 'string');
+            echo $taisarv . ' + ' . $taisarv . ' = ' . ($taisarv + $taisarv);
+            echo '<br>';
+            echo gettype($taisarv);
+    }
+
+    echo '      
         </body> 
     </html>
-';
-
+    ';
 ?>
