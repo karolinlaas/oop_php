1417cceee13889848b3854669590a824fade0df2
karolin.laas <karolinlaas@hotmail.com>
2018-12-13 10:52:34 +0200

    Tabeli klassi põhjal tehtud objekt

---
+++ tabeliTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karolin.laas
+ * Date: 13.12.2018
+ * Time: 10:44
+ */
+//  lugesin sisse Tabel.php faili sisu (importimine)
+require_once './Tabel.php';
+
+//  tabeli objekti valmistamine
+$lihtTabel = new Tabel();
+
+//  lisame read sisuga
+$lihtTabel->lisaRida(array(1, 2, 3));
+$lihtTabel->lisaRida(array(4, 5, 6));
+$lihtTabel->lisaRida(array(7, 8, 9));
+
+//  tabeli väljastamine
+$lihtTabel->naitaTabel();
