41c2ee3fd6b39c50806aac02b182f9ef541b0095
karolin.laas <karolinlaas@hotmail.com>
2018-12-13 11:23:50 +0200

    HTMLTabel - override, et oleks HTML tabel

--- HTMLTabel.php
+++ HTMLTabel.php
@@ -9,5 +9,30 @@
 require_once './Tabel.php';
 class HTMLTabel extends Tabel
 {
+    /**
+     * HTMLTabel constructor.
+     */
+    public function __construct()
+    {
+            echo 'HTML';
+            parent::__construct();
+    }
+
+    /*
+ * vaata tabelit massiiv foreach abil ridade kaupa ja väljasta igal real olev element HTML tabelina
+ * */
+    function naitaTabel()
+    {
+        echo '<table border = 1px>';
+        foreach ($this->tabel as $rida) {
+            echo '<tr>';
+            foreach ($rida as $element) {
+                echo '<td>'.$element.'</td>';
+            }
+            echo '</td>';
+        }
+        echo '</table>';
+    }
+
 
 }
