9cafe190d5f3c6402cc73ba58a0f92c95765639c
karolin.laas <karolinlaas@hotmail.com>
2018-12-18 08:59:27 +0200

    Massiivid - ühemõõtmeline sidestatud massiiv

--- katse.php
+++ katse.php
@@ -6,13 +6,6 @@
  * Time: 11:08
  */
 
-//Funktsioonid
-/*
- * function funktsiooniNimi([$parameeter1], $parameeter2, ..., $paramN) {
- *      kõik tegevused,
- *      mis antud funktsiooni sees toimuvad
- * }
- * */
 
 // Massiivid
 // array();
@@ -21,65 +14,26 @@
  * $massiiv() = väärtus
  * */
 
-//funktsioon
-function htmlTabel($ridadeArv = 0, $veergudeArv = 0) {
-    $tabel = '<table>';
-    for ($reanumber = 1; $reanumber <= $ridadeArv; $reanumber++) {
-        $tabel = $tabel.'<tr>';
-        for ($veeruNumber = 1; $veeruNumber <= $veergudeArv; $veeruNumber++) {
-            $tabel = $tabel.'<td>';
-            $tabel = $tabel.$reanumber;
-            $tabel = $tabel.'</td>';
-        }
-        $tabel = $tabel.'</tr>';
-    }
-    $tabel = $tabel.'</table>';
-    return $tabel;
-}
+// Massiivi loomine
+$porsasPeppa = array(
+    'nimi' => 'Peppa',
+    'amet' => 'põrsaslaps',
+    'vanus' => 5,
+    'sugu' => 'naine');
 
-//massiivi loomine
-$numbrid = array(
-    array(1, 2, 3),
-    array(4, 5, 6)
+$porsasGeorg = array(
+    'nimi' => 'Georg',
+    'amet' => 'põrsaslaps',
+    'vanus' => 2,
+    'sugu' => 'mees'
 );
 
-// massiivi sisu testkontroll
-var_dump($numbrid);
-echo '<pre>';
-print_r($numbrid);
-echo '</pre>';
-
-// massiivi elemendi väljastamine
-echo $numbrid[1][1];
-echo '<br>';
-
-// massiivi suuruse kontoll - ridade arv
-$ridadeArv = count($numbrid);
-echo $ridadeArv;
-echo '<br>';
-
-// veergudeArv
-$veergudeArv = count($numbrid[0]);
-echo $veergudeArv;
-echo '<br>';
-
-//massiivi väljastamine for'iga
-for($reaNumber = 0; $reaNumber < $ridadeArv; $reaNumber++) {
-    for($veeruNumber = 0; $veeruNumber < $veergudeArv; $veeruNumber++) {
-        echo $numbrid[$reaNumber][$veeruNumber].'&nbsp';
+// massiivi väljastamine
+function valjastaInfo($massiiv) {
+    foreach($massiiv as $elemendiNimi=>$elemendiVaartus) {
+        echo $elemendiNimi.' - '.$elemendiVaartus.'<br>';
     }
-    echo '<br>';
 }
-
-// massiivi väljastamine foreach'iga
-foreach ($numbrid as $rida) {
-    foreach ($rida as $number) {
-        echo $number.'&nbsp';
-    }
-    echo '<br>';
-}
-
-
 
 // lehe sisu väljastamine
 echo '
@@ -91,15 +45,9 @@ echo '
     </head> 
     <body>';
 
-// funktsiooni tööle kutsumine
-$tabel1 = htmlTabel(4, 4);
-echo $tabel1;
-echo '<hr>';
-$tabel2 = htmlTabel(2, 5);
-echo $tabel2;
-echo '<hr>';
-$tabel3 = htmlTabel(1, 2);
-echo $tabel3;
+valjastaInfo($porsasPeppa);
+echo '<br>';
+valjastaInfo($porsasGeorg);
 
 
 
