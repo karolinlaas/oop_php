6fe24788a37ff78f860a92e6abf360ee765ed3f7
karolin.laas <karolinlaas@hotmail.com>
2018-12-18 10:49:13 +0200

    Vorm.php ja selle töötlemise fail posti kasutades

--- katse.php
+++ katse.php
@@ -1,91 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: karolin.laas
- * Date: 06.12.2018
- * Time: 11:08
- */
-
-
-// Massiivid
-// array();
-/*
- * $massiiv = array() // tühi massiiv, siin ei ole midagi
- * $massiiv() = väärtus
- * */
-
-//sort($perekondPeppa); //tähestiku järjekorras
-//asort($perekondPeppa); //tähestiku järjekord aga nimed jätab alles
-//ksort($perekondPeppa); //võtmete järgi sorteerimine
-
-// massiivi väljastamine - funktsioon
-function suguVarv($sugu) {
-    if ($sugu == 'naine') {
-        echo '<div style="color: red">';
-    } else {
-        echo '<div style="color: blue">';
-    }
-}
-
-function valjastaInfo($massiiv) {
-    foreach ($massiiv as $alamMassiivNimi => $alamMassiivAndmed) {
-        suguVarv($alamMassiivAndmed['sugu']);
-        echo '<b>'.$alamMassiivNimi.'</b>';
-        foreach ($alamMassiivAndmed as $elemendiNimi => $elemendiVaartus) {
-            suguVarv($alamMassiivAndmed['sugu']);
-            echo $elemendiNimi.' - '.$elemendiVaartus.'</div>';
-        }
-        echo '<hr>';
-    }
-}
-
-function suguVordlus($porsas1, $porsas2) {
-    if ($porsas1['sugu'] == $porsas2['sugu']) {
-        return 0; }
-    return ($porsas1['sugu'] < $porsas2['sugu']) ? -1 : 1;
-}
-
-// Massiivi loomine
-$perekondPeppa = array(
-    'Peppa' => array(
-        'nimi' => 'Peppa',
-        'amet' => 'põrsaslaps',
-        'vanus' => 5,
-        'sugu' => 'naine'
-    ),
-    'Georg' => array(
-        'nimi' => 'Georg',
-        'amet' => 'põrsaslaps',
-        'vanus' => 2,
-        'sugu' => 'mees'
-    ),
-    'Põrsas Ema' => array(
-        'nimi' => 'Ema Põrsas',
-        'amet' => 'põrsasema',
-        'vanus' => 35,
-        'sugu' => 'naine'
-    ),
-    'Põrsas Isa' => array(
-        'nimi' => 'Isa Põrsas',
-        'amet' => 'põrsasisa',
-        'vanus' => 37,
-        'sugu' => 'mees'
-    )
-);
-
-uasort($perekondPeppa, 'suguVordlus');
 
 // lehe sisu väljastamine
 echo '
 <!doctype html>
 <html>
     <head>
-        <title> Massiiv </title>
+        <title> Vormid </title>
         <link type="text/css" rel="stylesheet" href="style.css">
     </head> 
     <body>';
 
-valjastaInfo($perekondPeppa);
 
 echo '      
     </body> 
---
+++ vorm.php
@@ -0,0 +1,39 @@
+<style>
+    form {
+        margin: 30px;
+        padding: 10px;
+        border: solid 1px black;
+        width: 300px;
+        text-align: center;
+    }
+
+    input {
+        margin-top: 5px;
+        margin-bottom: 10px;
+        border-radius: 15px;
+        border-style: solid;
+        border-width: 0.5px;
+        padding: 5px;
+    }
+
+
+</style>
+
+<form method="post" action="vormitootlus.php">
+    <div>
+        <label>Kasutajanimi:</label>
+        <br>
+        <input type="text" name="username">
+    </div>
+
+    <div>
+        <label>Parool:</label>
+        <br>
+        <input type="password" name="passwd">
+    </div
+
+    <div>
+        <input type="submit" value="Logi sisse">
+    </div>
+
+</form>
