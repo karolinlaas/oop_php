31ddaffc065a413485c4a738664db52183c8c1bd
karolin.laas <karolinlaas@hotmail.com>
2018-12-19 10:42:34 +0200

    login.php fail sisselogimise vormiga

---
+++ session/login.php
@@ -0,0 +1,24 @@
+<?php
+// session/login.php
+?>
+
+<form method="post" action="login_do.php">
+    <div>
+        <div>
+            <label>Kasutajanimi</label>
+            <input type="text" name="username">
+        </div>
+
+        <div>
+            <label>Parool</label>
+            <input type="password" name="password">
+        </div>
+
+        <div>
+            <input type="submit" value="Logi sisse">
+        </div>
+
+
+    </div>
+</form>
+
---
+++ session/login_do.php
@@ -0,0 +1,2 @@
+<?php
+// session/login_do.php
